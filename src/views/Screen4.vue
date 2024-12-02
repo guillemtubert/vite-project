4ee606c6594367e7f141fdb7460b5dbc74936202
Guillem <gtubert@roicos.com>
2024-12-02 18:32:19 +0100

    update title and dependencies; add vue-router and improve styles

--- src/views/Screen4.vue
+++ src/views/Screen4.vue
@@ -2,7 +2,10 @@
   <PasswordGuard correctPassword="pitakarp" title="Final Boss">
     <div class="screen">
       <h1>Ja quasi ho teniu :D</h1>
-      <p>Heu vist aquestes lletres per algun lloc? Potser en les etapes anteriors...</p>
+      <p>
+        Heu vist aquestes lletres per algun lloc? Potser en les etapes
+        anteriors...
+      </p>
       <p>al meu quart calaix potser hi ha algo...</p>
       <img src="../assets/imgs/selda.png" alt="columns" />
       <div class="fixed-letter">S</div>
@@ -28,5 +31,19 @@ import PasswordGuard from "../components/PasswordGuard.vue";
   font-weight: bold;
   color: black; /* You can change the color as desired */
   border-bottom: 2px solid red; /* Optional: Add a border */
+  animation: parpadeo 1s infinite; /* Duración de 1 segundo, se repite infinitamente */
+}
+
+h1 {
+}
+
+@keyframes parpadeo {
+  0%,
+  100% {
+    opacity: 1; /* Totalmente visible */
+  }
+  50% {
+    opacity: 0; /* Totalmente invisible */
+  }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-project/',
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
